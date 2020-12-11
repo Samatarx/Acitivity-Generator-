d7a390f0aba39710bc8050673044ae0c2d8446d1
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-11 16:08:49 +0000

    Change to https and formatting

--- script.js
+++ script.js
@@ -5,33 +5,32 @@ activBtn.addEventListener("click", generateActivity);
 
 generateActivity();
 
-
 // using async await
-async function generateActivity() {
-  const config = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-
-  const response = await fetch("http://www.boredapi.com/api/activity/", config);
-
-  const data = await response.json()
-
-  activityEl.innerHTML = data.activity
-}
-
-// using .then()
-// function generateJoke() {
+// async function generateActivity() {
 //   const config = {
 //     headers: {
 //       Accept: "application/json",
 //     },
 //   };
 
-//   fetch("https://icanhazdadjoke.com", config)
-//     .then((response) => response.json())
-//     .then((data) => {
-//         jokeEl.innerHTML = data.joke
-//     });
+//   const response = await fetch("http://www.boredapi.com/api/activity/", config);
+
+//   const data = await response.json()
+
+//   activityEl.innerHTML = data.activity
 // }
+
+using.then();
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  fetch("https://icanhazdadjoke.com", config)
+    .then((response) => response.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+}
